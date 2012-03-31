ef387c9d2eeebe769f40cb503c743d1e2ec59acc
Matt McGlincy <mattmcglincy@gmail.com>
2012-03-30 20:55:42 -0700

    Initial cocos2d project.

---
+++ Molyjam/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  Molyjam
+//
+//  Created by Matthew McGlincy on 3/30/12.
+//  Copyright n/a 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
